40c05843d87f8b8b2476252dd67aad768854c4ff
FatimaGillaniShah <fatimagillanishah@gmail.com>
2025-07-31 15:25:38 +1000

    Final clean implementation of getArnieQuotes

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,22 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  // Create concurrent requests for all URLs
+  const requests = urls.map(async (url) => {
+    const response = await httpGet(url);
+    const body = JSON.parse(response.body);
+
+    // Return success or failure object based on HTTP status
+    if (response.status === 200) {
+      return { "Arnie Quote": body.message };
+    } else {
+      return { FAILURE: body.message };
+    }
+  });
+
+  // Wait for all requests to complete
+  const results = await Promise.all(requests);
+  return results;
 };
 
 module.exports = {
